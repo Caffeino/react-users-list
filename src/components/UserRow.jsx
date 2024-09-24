348ee56c1b9a54b44fad87001f1812c717a7b0cf
Andres Tellez <andres.tezper@gmail.com>
2024-09-24 00:12:47 -0600

    UsersList with context hook

--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -1,24 +1,30 @@
+import { useContext } from 'react';
+import { UsersContext } from '../lib/context/UsersContex';
 import UserRole from './UserRole';
 import style from './UserRow.module.css';
 import UserStatus from './UserStatus';
 
-const UserRow = ({ id, name, active, role, toggleUsersActive }) => (
-	<div className={style.user}>
-		<div className={style.name}>
-			<span>{name}</span>
+const UserRow = ({ id, name, active, role }) => {
+	const { toggleUsersActive } = useContext(UsersContext);
+
+	return (
+		<div className={style.user}>
+			<div className={style.name}>
+				<span>{name}</span>
+			</div>
+			<div className={style.status}>
+				<UserStatus status={active} />
+			</div>
+			<div className={style.role}>
+				<UserRole role={role} />
+			</div>
+			<div className={style.action}>
+				<button onClick={() => toggleUsersActive(id)}>
+					{active ? 'Inactive' : 'Active'}
+				</button>
+			</div>
 		</div>
-		<div className={style.status}>
-			<UserStatus status={active} />
-		</div>
-		<div className={style.role}>
-			<UserRole role={role} />
-		</div>
-		<div className={style.action}>
-			<button onClick={() => toggleUsersActive(id)}>
-				{active ? 'Inactive' : 'Active'}
-			</button>
-		</div>
-	</div>
-);
+	);
+};
 
 export default UserRow;
--- src/components/UsersListRows.jsx
+++ src/components/UsersListRows.jsx
@@ -1,11 +1,9 @@
 import UserRow from './UserRow';
 
-const UsersListRows = ({ users, toggleUsersActive }) => {
+const UsersListRows = ({ users }) => {
 	if (!users.length) return <p>There are no users to display yet.</p>;
 
-	return users.map(user => (
-		<UserRow key={user.id} toggleUsersActive={toggleUsersActive} {...user} />
-	));
+	return users.map(user => <UserRow key={user.id} {...user} />);
 };
 
 export default UsersListRows;
