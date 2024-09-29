bdd4ea5a8aeeea24fb1a02357eb76a8e180c3211
Andres Tellez <andres.tezper@gmail.com>
2024-09-28 23:37:16 -0600

    Users data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
 import UsersList from './components/UsersList';
-import { USER_ROLES } from './constants/userRoles';
-
-const USERS = [
-	{
-		username: 'andyt',
-		name: 'Andres Téllez',
-		active: true,
-		role: USER_ROLES.OTHER
-	},
-	{
-		username: 'pablitop',
-		name: 'Pablo Perez',
-		active: false,
-		role: USER_ROLES.TEACHER
-	},
-	{
-		username: 'javij',
-		name: 'Javier Juarez',
-		active: true,
-		role: USER_ROLES.STUDENT
-	}
-];
 
 function App() {
-	return <UsersList initialUsers={USERS} />;
+	return <UsersList />;
 }
 
 export default App;
--- src/components/UsersListRows.jsx
+++ src/components/UsersListRows.jsx
@@ -1,9 +1,11 @@
 import UserRow from './UserRow';
 
-const UsersListRows = ({ users }) => {
+const UsersListRows = ({ users, error, loading }) => {
+	if (loading) return <p>Loading users...</p>;
+	if (error) return <p>Error trying to load user list.</p>;
 	if (!users.length) return <p>There are no users to display yet.</p>;
 
-	return users.map(user => <UserRow key={user.username} {...user} />);
+	return users.map(user => <UserRow key={user.id} {...user} />);
 };
 
 export default UsersListRows;
