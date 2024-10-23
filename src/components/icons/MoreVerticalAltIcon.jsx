d6a7a7520bfd5402c0620f3301918edb1842f60c
Andres Tellez <andres.tezper@gmail.com>
2024-10-22 23:13:46 -0600

    Add new icons and colors

---
+++ src/components/icons/MoreVerticalAltIcon.jsx
@@ -0,0 +1,10 @@
+const MoreVerticalAltIcon = props => (
+	<svg {...props} fill='none' viewBox='0 0 24 24' height='1em' width='1em'>
+		<path
+			fill='currentColor'
+			d='M14 6a2 2 0 11-4 0 2 2 0 014 0zM14 12a2 2 0 11-4 0 2 2 0 014 0zM14 18a2 2 0 11-4 0 2 2 0 014 0z'
+		/>
+	</svg>
+);
+
+export default MoreVerticalAltIcon;
