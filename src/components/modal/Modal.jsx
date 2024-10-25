8955da03aee94b3da6fbf1bd9417d9c16263bde3
Andres Tellez <andres.tezper@gmail.com>
2024-10-25 00:19:46 -0600

    Modal scroll

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import IconButton from '../buttons/IconButton';
 import CrossCircleIcon from '../icons/CrossCircleIcon';
 import style from './Modal.module.css';
 
 const Modal = ({ closeModal, children }) => {
+	useEffect(() => {
+		if (!children) return;
+
+		document.body.classList.add(style.bodyOverflow);
+
+		return () => {
+			document.body.classList.remove(style.bodyOverflow);
+		};
+	}, [children]);
+
 	if (!children) return null;
 
 	return createPortal(
